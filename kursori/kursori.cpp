aec9ef4584a740b09242599f5e7c745966bcdbbe
Alexey Rusakov <rusal@bk.ru>
2019-12-13 17:41:09 +0300

    C++ Program for QuickSort

--- kursori/kursori.cpp
+++ kursori/kursori.cpp
@@ -50,6 +50,64 @@ bool setElement(int nomer, int element,  stack* st, KurList* kur1) {
 	}
 	return false;
 }
+
+// A utility function to swap two elements 
+void swap(stack* st, KurList* kur1, int i, int j)
+{
+	int ii = getElement(i, st, kur1);
+	int jj = getElement(j, st, kur1);
+	setElement(i, jj, st, kur1);
+	setElement(j, ii, st, kur1);
+}
+
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+	array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition(stack* st, KurList* kur1, int low, int high)
+{
+	//int pivot = arr[high];    // pivot 
+	int pivot = getElement(high, st, kur1);
+	int i = (low - 1);  // Index of smaller element 
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than or 
+		// equal to pivot 
+		//if (arr[j] <= pivot)
+		if (getElement(j, st, kur1) <= pivot)
+		{
+			i++;    // increment index of smaller element 
+			swap(st, kur1, i, j);
+			//swap(&arr[i], &arr[j]); 
+		}
+	}
+	//swap(&arr[i + 1], &arr[high]);
+	swap(st, kur1, i+1, high);
+	return (i + 1);
+}
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(stack* st, KurList* kur1, int low, int high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		   at right place */
+		int pi = partition(st, kur1, low, high);
+		//int pi = partition(arr, low, high);
+
+		// Separately sort elements before 
+		// partition and after partition 
+		quickSort(st, kur1, low, pi - 1);
+		quickSort(st, kur1, pi + 1, high);
+		//quickSort(arr, low, pi - 1);
+		//quickSort(arr, pi + 1, high);
+	}
+}
 int main()
 {
 	//List* ls = new List();
@@ -78,16 +136,18 @@ int main()
 		cout << st1->pop()<<"\n";
 	};
 	cout << "\n";*/
+	cout << "Unsorted array:\n";
 	st1->show();
 
 	int n = st1->size(); //n - количество элементов
-
-	//Пишем обертку
+	quickSort(st1, kur1, 0, n - 1);
+	printf("Sorted array: n");
+	st1->show();
+	//Пишем обертку - отладка
 	//int elem = getElement(5, st1, kur1);
 	//cout << "\n" << elem <<"\n";
-	setElement(0,777, st1, kur1);
-
-	st1->show();
+	//setElement(0,777, st1, kur1);
+	//st1->show();
 
 
 }
