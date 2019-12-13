4a1706c56759065c9104d4ff949b582dddba4e95
Alexey Rusakov <rusal@bk.ru>
2019-12-13 16:46:36 +0300

    obertka set

--- kursori/kursori.cpp
+++ kursori/kursori.cpp
@@ -9,16 +9,46 @@ using namespace std;
 int getElement(int nomer, stack* st, KurList* kur1){
 	if ((nomer >= 0) and (nomer < st->size()))
 	{
+	
 		stack* temp_stack = new stack;
 		temp_stack->init(kur1);
 		int pos = 0;
-		//while (pos <= nomer)
+		while (pos < nomer)
 		{
-			//temp_stack->push(st->pop());
-
+			temp_stack->push(st->pop());
+			pos++;
 		}
+		int element = st->peek();
+		while (not temp_stack->isempty())
+		{
+			st->push(temp_stack->pop());
+		}
+		return element;
 	}
 	return -1;
+}
+
+bool setElement(int nomer, int element,  stack* st, KurList* kur1) {
+	if ((nomer >= 0) and (nomer < st->size()))
+	{
+
+		stack* temp_stack = new stack;
+		temp_stack->init(kur1);
+		int pos = 0;
+		while (pos < nomer)
+		{
+			temp_stack->push(st->pop());
+			pos++;
+		}
+		st->pop();
+		st->push(element);
+		while (not temp_stack->isempty())
+		{
+			st->push(temp_stack->pop());
+		}
+		return true;
+	}
+	return false;
 }
 int main()
 {
@@ -32,13 +62,12 @@ int main()
 
 	stack* st1 = new stack;
 	st1->init(kur1);
-	st1->push(5);
-	st1->push(2);
+	st1->push(10);
+	st1->push(7);
 	st1->push(8);
-	st1->push(16);
+	st1->push(9);
 	st1->push(1);
-	st1->push(19);
-	st1->push(22);
+	st1->push(5);
 	/*cout << st1->peek() << "\n";  // для отладки
 	cout << "stack!\n";
 	kur1->showmas();
@@ -54,7 +83,11 @@ int main()
 	int n = st1->size(); //n - количество элементов
 
 	//Пишем обертку
+	//int elem = getElement(5, st1, kur1);
+	//cout << "\n" << elem <<"\n";
+	setElement(0,777, st1, kur1);
 
+	st1->show();
 
 
 }
